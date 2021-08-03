7f2e5195732757075cfea2202c85bc9fea6e1e0b
Jessamyn McTwigan <jessamyn.mctwigan@gmail.com>
2021-08-02 20:06:24 -0500

    adding all files from activity 20 and converting gallery to blog and painting to comment

---
+++ models/index.js
@@ -0,0 +1,13 @@
+const User = require('./User');
+const Blog = require('./Blog');
+const Comment = require('./Comment');
+
+Blog.hasMany(Comment, {
+    foreignKey: 'blog_id',
+});
+
+Comment.belongsTo(Blog, {
+    foreignKey: 'blog_id',
+});
+
+module.exports = { User, Blog, Comment };
